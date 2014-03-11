8e53bf711adf77fcb816fc3b2cd5e546784fb8bf
Mark Miyashita <negativetwelve@gmail.com>
2014-03-10 20:08:55 -0700

    Add initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsStarterApp::Application.config.secret_key_base = 'b9e0a80d0a845425dd8aa5b19c0b18f49c74ace1d7b164a44d2185aa4b4164104a410c8ea3e80f5d804d6b4bdb9a90a4add1bbdf3d5003c43fdebf7f170f5d13'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsStarterApp::Application.config.session_store :cookie_store, key: '_rails-starter-app_session'
